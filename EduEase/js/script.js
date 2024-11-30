b9febe8817a1438e284499434510c071b23b5557
Shivasubrahmanya K C <shivakc004@gmail.com>
2024-11-30 16:50:18 +0530

    Add files via upload

---
+++ EduEase/js/script.js
@@ -0,0 +1,82 @@
+// Dark Mode Toggle
+let toggleBtn = document.getElementById('toggle-btn');
+let body = document.body;
+let darkMode = localStorage.getItem('dark-mode');
+
+const enableDarkMode = () => {
+   toggleBtn.classList.replace('fa-sun', 'fa-moon');
+   body.classList.add('dark');
+   localStorage.setItem('dark-mode', 'enabled');
+}
+
+const disableDarkMode = () => {
+   toggleBtn.classList.replace('fa-moon', 'fa-sun');
+   body.classList.remove('dark');
+   localStorage.setItem('dark-mode', 'disabled');
+}
+
+if (darkMode === 'enabled') {
+   enableDarkMode();
+}
+
+toggleBtn.onclick = (e) => {
+   darkMode = localStorage.getItem('dark-mode');
+   if (darkMode === 'disabled') {
+      enableDarkMode();
+   } else {
+      disableDarkMode();
+   }
+}
+
+// Profile Toggle
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () => {
+   profile.classList.toggle('active');
+   search.classList.remove('active');
+}
+
+// Search Toggle
+let search = document.querySelector('.header .flex .search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+   search.classList.toggle('active');
+   profile.classList.remove('active');
+}
+
+// Sidebar Toggle
+let sideBar = document.querySelector('.side-bar');
+
+document.querySelector('#menu-btn').onclick = () => {
+   sideBar.classList.toggle('active');
+   body.classList.toggle('active');
+}
+
+document.querySelector('#close-btn').onclick = () => {
+   sideBar.classList.remove('active');
+   body.classList.remove('active');
+}
+
+// Window Scroll Handling
+window.onscroll = () => {
+   profile.classList.remove('active');
+   search.classList.remove('active');
+
+   if (window.innerWidth < 1200) {
+      sideBar.classList.remove('active');
+      body.classList.remove('active');
+   }
+}
+
+// Audiobook Dropdown Toggle
+let exploreButton = document.getElementById('explore-audiobooks');
+let dropdown = document.getElementById('audiobooks-dropdown');
+
+exploreButton.addEventListener('click', function() {
+   // Toggle dropdown visibility
+   if (dropdown.style.display === 'none') {
+      dropdown.style.display = 'block';
+   } else {
+      dropdown.style.display = 'none';
+   }
+});
